7ca8cb30451e7d815684df7b7f417252f15c3c19
nikolaevv <starlei165@gmail.com>
2021-09-17 19:21:47 +0300

    Recreate database

--- db_create.py
+++ db_create.py
@@ -1,14 +1,17 @@
 import datetime
 
-from app import models
-from app.database import SessionLocal, engine
+import models
+from database import SessionLocal, engine
 
 db = SessionLocal()
 
 models.Base.metadata.create_all(bind=engine)
 
 db_record = models.Message(
-    
+    text="Здравствуйте, я произвела частичное досрочное погашение кредита, но у меня уменьшился срок кредита, а не перерасчёт процентов, как сделать чтобы был именно платёж уменьшался?",
+    timestamp=datetime.datetime.now(),
+    assensment=50,
+    sender="USER"
 )
 
 db.add(db_record)
